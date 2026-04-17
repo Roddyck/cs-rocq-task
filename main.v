78d5e348cd94a30e2000354be58b686ae3613d83
Roddyck <rodion.ivashchenko2003@gmail.com>
2026-04-17 15:47:36 +0300

    go back to initial specs, write functions according to those specs, rewrite proofs with induction step as separate lemmas and add some comments

--- main.v
+++ main.v
@@ -19,70 +19,120 @@ Tactic Notation "bdestruct" constr(X) "as" ident(H) := bdestr X H.
 
 Section Search1.
 
+(** Функция [array] моделирует массив, где индексы строго меньше некоторого
+параметра [n], т.о. [n] - длина массива *)
 Variable array : nat -> nat.
 
+(** для пустого массива ([n = 0]) возвращаем false *)
 Fixpoint search1 (n x : nat) : bool :=
   match n with
-  | 0 => x =? array 0
-  | S k => (x =? array n) || search1 k x
+  | 0 => false
+  | S k => (x =? array k) || search1 k x 
   end.
 
+Lemma search1Spec_forward_step :
+  forall n x,
+  ((exists i, i < n /\ array i = x) -> search1 n x = true) ->
+  ((exists i, i < S n /\ array i = x) -> search1 (S n) x = true).
+Proof.
+  intros n x IH.
+  intros [i [H1 H2]]. simpl. assert (i < n \/ i = n) by lia.
+  destruct H as [Hlt | Heq].
+  * bdestruct (x =? array n); [reflexivity |]. simpl. apply IH. exists i. lia.
+  * bdestruct (x =? array n); [reflexivity |]. simpl. apply IH. exists i. subst i. lia.
+Qed.
+
+Lemma search1Spec_backward_step :
+  forall n x,
+  (search1 n x = true -> (exists i, i < n /\ array i = x)) ->
+  (search1 (S n) x = true -> (exists i, i < S n /\ array i = x)).
+Proof.
+  intros n x IH.
+  intros H. simpl in H. bdestruct (x =? array n).
+  * exists n. lia.
+  * simpl in H. apply IH in H. destruct H as [i [H1 H2]].
+    exists i. split; lia.
+Qed.
+
 Theorem search1Spec :
-  forall n x, (exists i, i <= n /\ array i = x) <-> search1 n x = true.
+  forall n x, (exists i, i < n /\ array i = x) <-> search1 n x = true.
 Proof.
   split.
   * induction n as [| n IH].
-    - intros [i [H1 H2]]. simpl. assert (i = 0) by lia. subst x i. apply eqb_refl.
-    - intros [i [H1 H2]]. simpl. unfold "||". bdestruct (x =? array (S n));
-      [reflexivity |]. assert (i <= n \/ i = S n) by lia. destruct H0 as [H0 | H0].
-      + apply IH. exists i. lia.
-      + apply IH. exists i. subst i. lia.
+    - intros [i [H1 H2]]. lia.
+
+    - apply search1Spec_forward_step. exact IH. 
 
   * induction n as [| n IH].
-    - intros H. exists 0. simpl in H. bdestruct (x =? array 0); lia.
-    - intros H. simpl in H. bdestruct (x =? array (S n)).
-      + simpl in H. exists (S n). split; lia.
-      + simpl in H. apply IH in H. destruct H as [i [H1 H2]]. exists i. lia.
+    - intros H. simpl in H. discriminate H.
+
+    - apply search1Spec_backward_step. exact IH.
 Qed.
 
 End Search1.
 
 Section Search2.
 
+(** [array2] моделирует двумерный массив, где [m] и [n] количество строк и
+столбцов соответственно. *)
 Variable array2 : nat -> nat -> nat.
 
+(** аналогично одномерному массиву, при [m = 0] массив пуст и возвращаем false *)
 Fixpoint search2 (m n x : nat) : bool :=
   match m with
-  | 0 => search1 (array2 0) n x
-  | S k => search1 (array2 m) n x || search2 k n x
+  | 0 => false 
+  | S k => search1 (array2 k) n x || search2 k n x
   end.
+
+Lemma search2Spec_forward_step :
+  forall m n x,
+  ((exists j k, j < m /\ k < n /\ array2 j k = x) -> search2 m n x = true) ->
+  ((exists j k, j < S m /\ k < n /\ array2 j k = x) -> search2 (S m) n x = true).
+Proof.
+  intros m n x IH.
+  intros [j [k [H1 [H2 H3]]]]. simpl. assert (j < m \/ j = m) by lia.
+  destruct H as [Hlt | Heq].
+
+  * destruct (search1 (array2 m) n x) eqn:Hb; [reflexivity |]. simpl. 
+    apply IH. exists j, k. lia.
+
+  * destruct (search1 (array2 m) n x) eqn:Hb; [reflexivity |]. simpl.
+    (** противоречивый случай ([j = m]) и при этом [search1 (array2 m) n x = false] *)
+    (** докажем, что должно быть [search1 (array2 m) n x = true] *)
+    assert (search1 (array2 m) n x = true) as Htrue.
+    - rewrite <- search1Spec. exists k. subst j. split; assumption.
+    - rewrite Hb in Htrue. discriminate Htrue.
+Qed.
+
+Lemma search2Spec_backward_step :
+  forall m n x,
+  (search2 m n x = true -> (exists j k, j < m /\ k < n /\ array2 j k = x)) ->
+  (search2 (S m) n x = true -> (exists j k, j < S m /\ k < n /\ array2 j k = x)).
+Proof.
+  intros m n x IH.
+  intros H. simpl in H. destruct (search1 (array2 m) n x) eqn:Hb.
+
+  * rewrite <- search1Spec in Hb. destruct Hb as [k [H1 H2]].
+    exists m, k. split; lia.
+
+  * simpl in H. apply IH in H. destruct H as [j [k [H1 [H2 H3]]]].
+    exists j, k. lia.
+Qed.
 
 Theorem search2Spec :
   forall m n x,
-  (exists j k, j <= m /\ k <= n /\ array2 j k = x) <-> search2 m n x = true.
+  (exists j k, j < m /\ k < n /\ array2 j k = x) <-> search2 m n x = true.
 Proof.
   split.
   * induction m as [| m IH].
-    - intros [j [k [H1 [H2 H3]]]]. simpl.
-      assert (j = 0) by lia. subst j. apply search1Spec. exists k. split; assumption.
-      
-    - intros [j [k [H1 [H2 H3]]]]. simpl. 
-      destruct (search1 (array2 (S m)) n x) eqn:Hb; [reflexivity |].
-      rewrite le_succ_r in H1. destruct H1 as [Hle | Heq]. 
-      + simpl. apply IH. exists j, k. lia.
-      + simpl. subst j. assert (search1 (array2 (S m)) n x = true) as Htrue.
-        { rewrite <- search1Spec. exists k. split; assumption. }
-        rewrite Hb in Htrue. discriminate Htrue.
+    - intros [j [k [H1 [H2 H3]]]]. lia.
+
+    - apply search2Spec_forward_step. exact IH.
 
   * induction m as [| m IH].
-    - intros H. simpl in H. rewrite <- search1Spec in H.
-      destruct H as [k [H1 H2]]. exists 0, k. split; lia.
-      
-    - intros H. simpl in H. destruct (search1 (array2 (S m)) n x) eqn:Hb.
-      + rewrite <- search1Spec in Hb. destruct Hb as [k [H1 H2]].
-        exists (S m), k. split; lia.
-      + simpl in H. apply IH in H. destruct H as [j [k [H1 [H2 H3]]]].
-        exists j, k. lia.
+    - intros H. simpl in H. discriminate H.
+
+    - apply search2Spec_backward_step. exact IH.
 Qed.
 
 End Search2.
